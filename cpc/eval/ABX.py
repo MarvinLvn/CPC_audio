63d98086beb28ee30bb92e9cb7b3c976087f91f4
MarvinLvn <marvinlavechin@gmail.com>
2020-06-20 18:39:49 +0200

    Add confusion matrices in .json returned by ABX

--- cpc/eval/ABX.py
+++ cpc/eval/ABX.py
@@ -62,7 +62,8 @@ def ABX(feature_function,
         divisor_speaker = index_speaker.sum(dim=0)
         phone_confusion = reduce_sparse_data(group_confusion.sum(dim=0),
                                              divisor_speaker)
-
+        scores['within_phone_confusion'] = phone_confusion.tolist()
+        scores['phones'] = ABXDataset.get_phones()
         scores['within'] = (phone_confusion.sum() /
                             (divisor_speaker > 0).sum()).item()
         print(f"...done. ABX within : {scores['within']}")
@@ -90,6 +91,7 @@ def ABX(feature_function,
         divisor_speaker = index_speaker.sum(dim=0)
         phone_confusion = reduce_sparse_data(group_confusion.sum(dim=0),
                                              divisor_speaker)
+        scores['across_phone_confusion'] = phone_confusion.tolist()
         scores['across'] = (phone_confusion.sum() /
                             (divisor_speaker > 0).sum()).item()
         print(f"...done. ABX across : {scores['across']}")
@@ -166,6 +168,7 @@ def main(argv):
 
     if args.load == 'from_checkpoint':
         # Checkpoint
+        epoch = Path(args.path_checkpoint).stem.replace('checkpoint_', '')
         model = loadModel([args.path_checkpoint])[0]
         model.gAR.keepHidden = True
         # Feature maker
@@ -176,6 +179,7 @@ def main(argv):
                                                      strict=args.strict,
                                                      maxSizeSeq=args.max_size_seq)
     elif args.load == 'from_pre_computed':
+        epoch = 'precomputed'
         def feature_function(x): return torch.load(x, 'cpu')
 
     # Modes
@@ -208,14 +212,10 @@ def main(argv):
         else Path(args.out)
     out_dir.mkdir(exist_ok=True)
 
-    path_score = out_dir / 'ABX_scores.json'
+    scores['args'] = vars(args)
+    path_score = out_dir / 'ABX_checkpoint_{}.json' .format(epoch)
     with open(path_score, 'w') as file:
         json.dump(scores, file, indent=2)
-
-    path_args = out_dir / 'ABX_args.json'
-    with open(path_args, 'w') as file:
-        json.dump(vars(args), file, indent=2)
-
 
 if __name__ == "__main__":
     args = sys.argv[1:]
